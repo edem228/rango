871e3f958379b7e500e863b5fb473164fc235527
Edem <edem228@gmail.com>
2016-03-09 18:16:42 +0100

    Bug trying to install django-markdown

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
+from django_markdown.admin import MarkdownModelAdmin
 
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(MarkdownModelAdmin):
     list_display = ("title", "created_at")
     prepopulated_fields = {"slug": ("title",)}
 admin.site.register(models.Article, ArticleAdmin)
